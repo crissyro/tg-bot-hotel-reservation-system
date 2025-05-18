30070de67b486812b83904ac9b94d57464eb0013
Moroz Roman <morozdef24@gmail.com>
2025-05-18 15:03:55 +0300

    make keyboard

--- bot/handlers/user/booking.py
+++ bot/handlers/user/booking.py
@@ -3,10 +3,11 @@ import logging
 from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import StatesGroup, State
-from datetime import datetime
+from datetime import datetime, timedelta
 from services.postgres_crud import BookingCRUD, RoomCRUD
 from services.postgres_database import PostgresDatabase
-from keyboards.user import main_keyboard, back_keyboard
+from keyboards.user import main_keyboard
+from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from config.config import config
 
 booking_router = Router()
@@ -17,3 +18,12 @@ class BookingStates(StatesGroup):
     SELECT_ROOM = State()
     CONFIRM_BOOKING = State()
     PAYMENT = State()
+
+async def date_keyboard():
+    builder = ReplyKeyboardBuilder()
+    today = datetime.today()
+    for i in range(1, 31):
+        date = today + timedelta(days=i)
+        builder.button(text=date.strftime("%d.%m.%Y"))
+    builder.adjust(4)
+    return builder.as_markup(resize_keyboard=True)
